ab0d4eb103fda9df37f72b95d8e8a6633c0817ff
Leonid Onokhov <sopvop@gmail.com>
2013-10-22 13:13:54 +0400

    Update Entity

--- src/Database/PostgreSQL/Simple/Dsl/Entity.hs
+++ src/Database/PostgreSQL/Simple/Dsl/Entity.hs
@@ -9,34 +9,17 @@
 
 
 module Database.PostgreSQL.Simple.Dsl.Entity
-    (
-    {-
-     Entity(..)
-    , Storable(..)
-    , Update
-    , (=.)
-    , setField
-    , getEntity
+    ( getEntity
     , updateEntity
-    , updateEntityReturning
     , insertEntity
-    , insertFieldsReturning
-    -}
+    , deleteEntity
     ) where
 
-import           Control.Monad                           (liftM, void)
-
-import           Blaze.ByteString.Builder.ByteString     as B
-import           Blaze.ByteString.Builder.Char8          as B
-import           Data.ByteString                         (ByteString)
-import           Data.List                               (intersperse)
-import           Data.Monoid
-import           Data.Proxy
-import           GHC.TypeLits                            (SingI)
+import           Control.Monad                           (void)
 
 import           Database.PostgreSQL.Simple              ((:.) (..), Connection,
-                                                          Only (..), execute,
-                                                          query)
+                                                          Only (..))
+import           Database.PostgreSQL.Simple.Dsl
 import           Database.PostgreSQL.Simple.Dsl.Internal
 import           Database.PostgreSQL.Simple.FromField    (FromField)
 import           Database.PostgreSQL.Simple.ToField      (Action (..),
@@ -44,82 +27,40 @@ import           Database.PostgreSQL.Simple.ToField      (Action (..),
 
 class (Selectable a, Table a, ToField (EntityId a), FromField (EntityId a)) => Entity a where
    type EntityId a :: *
-   entityIdColumn :: Proxy a -> ByteString
-   entityIdColumn _ = "id"
+   idField :: Expr (Whole a) -> Expr (EntityId a)
 
 class (Entity a) => Storable a where
-   entityUpdate :: a -> Update a
+   entityUpdate :: a -> UpdExpr a
 
-{-
+
 takeOne :: Monad m => [Whole a] -> m (Maybe a)
 takeOne [] = return Nothing
 takeOne (x:_) = return (Just $ getWhole x)
-
+{-
 takeOneE :: Monad m => [a] -> m a
 takeOneE (x:_) = return x
 takeOneE _ = error "takeOneE: Need proper error here"
-
-makeColumnsAction :: [ByteString] -> Action
-makeColumnsAction bs = Plain . mconcat $ intersperse (B.fromChar ',') cols
-  where
-     cols = map B.fromByteString bs
-
-makeListOfActions :: [Action] -> Action
-makeListOfActions = Many . intersperse (plain ",")
-
-getIdAndTable :: Entity a => Proxy a -> (Action, Action)
-getIdAndTable p = (idcol, table)
-  where
-   idcol = Plain . B.fromByteString $ entityIdColumn p
-   table = Plain . B.fromByteString $ tableName p
-
-getEntity :: forall a . (Entity a) => Connection -> EntityId a -> IO (Maybe a)
-getEntity c eid = takeOne =<< query c "SELECT ? FROM ? WHERE ?=? LIMIT 1"
-                            [columns, table, idcol, toField eid]
-   where
-     (idcol, table) = getIdAndTable (undefined :: Proxy a)
-     columns = makeColumnsAction . entityColumns $ entityParser (undefined :: Proxy a)
-
--- | Update entity
-updateEntity :: forall a . (Entity a) => Connection -> EntityId a -> Update a -> IO ()
-updateEntity c eid (Update upds) =
-    void $ execute c "UPDATE ? SET (?)=(?) WHERE ?=?"
-            [table, columns, vals, idcol, toField eid]
-   where
-     columns = makeColumnsAction $ map fst upds
-     vals = makeListOfActions $ map snd upds
-     (idcol, table) = getIdAndTable (undefined :: Proxy a)
-
--- | Update and return whole entity
-updateEntityReturning :: forall a . (Entity a) => Connection -> EntityId a -> Update a -> IO a
-updateEntityReturning c eid (Update upds) =
-   liftM getWhole $ takeOneE =<< query c "UPDATE ? SET (?)=(?) WHERE ?=? RETURNING ?"
-            [table, columns, vals, idcol, toField eid, allColumns]
-   where
-     columns = makeColumnsAction $ map fst upds
-     vals = makeListOfActions $ map snd upds
-     (idcol, table) = getIdAndTable (undefined :: Proxy a)
-     allColumns = makeColumnsAction . entityColumns $ entityParser (undefined :: Proxy a)
-
--- | insert new entity
-insertEntity :: forall a . (Storable a) => Connection -> a -> IO (EntityId a)
-insertEntity c v = liftM fromOnly $ takeOneE =<< query c "INSERT INTO ? (?) VALUES (?)"
-             [table, columns, vals]
-  where
-     Update upds = entityUpdate v
-     columns = makeColumnsAction $ map fst upds
-     vals = makeListOfActions $ map snd upds
-     (_, table) = getIdAndTable (undefined :: Proxy a)
-
--- | Insert partial fields returning whole entity
-insertFieldsReturning :: forall a . (Entity a) => Connection -> Update a -> IO (EntityId a, a)
-insertFieldsReturning c (Update upds) =
-   liftM toResult $ takeOneE =<< query c "INSERT INTO ? (?) VALUES (?) RETURNING ?,?"
-            [table, columns, vals, idcol, allColumns]
-   where
-     columns = makeColumnsAction $ map fst upds
-     vals = makeListOfActions $ map snd upds
-     (idcol, table) = getIdAndTable (undefined :: Proxy a)
-     allColumns = makeColumnsAction . entityColumns $ entityParser (undefined :: Proxy a)
-     toResult (Only a :. Whole b) = (a,b)
 -}
+takeOnlyE :: Monad m => [Only a] -> m a
+takeOnlyE (Only x:_) = return x
+takeOnlyE _ = error "takeOnlyE: Need proper error here"
+
+getEntity :: Entity a => Connection -> EntityId a -> IO (Maybe a)
+getEntity c eid = takeOne =<< query c sel
+   where
+     sel = select . where_ (\en ->idField en ==. val eid) $ fromTable
+
+updateEntity :: Entity a => Connection -> EntityId a -> UpdExpr a -> IO ()
+updateEntity c eid upds = void $ executeUpdate c upd
+   where
+     upd = update (\u -> idField u ==. val eid) upds
+
+insertEntity :: Storable a => Connection -> a -> IO (EntityId a)
+insertEntity c ent = takeOnlyE =<< queryUpdate c ins
+   where
+    ins = returning (\e -> only (idField e)) $ insert (entityUpdate ent)
+
+deleteEntity :: forall a . Entity a => Connection -> EntityId a -> IO ()
+deleteEntity c eid = void $ executeUpdate c del
+  where
+    del = delete (\(u::Expr (Whole a)) -> idField u ==. val eid)
